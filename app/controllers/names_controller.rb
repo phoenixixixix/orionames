15a98f3f687c8c0f6565f915ca2656bebbb44f3c
phoenixixixix <barchukoleg12@gmail.com>
2022-11-03 17:22:53 +0200

    Add NamesController and Initialize Views

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,24 @@
+class NamesController < ApplicationController
+  def index
+    @names = Name.all
+  end
+
+  def new
+    @name = Name.new
+  end
+
+  def create
+    @name = Name.new(name_params)
+    if @name.save
+      redirect_to names_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def name_params
+    params.require(:name).permit(:title, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "pages#main"
-
+  
   get "/main", to: "pages#main"
+    
+  resources :names
 end
