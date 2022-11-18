d436b888d78f4f0a28d0149ea3c491c258e110d5
phoenixixixix <barchukoleg12@gmail.com>
2022-11-18 16:33:34 +0200

    Add main page block relative to NameDays

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,9 @@ class PagesController < ApplicationController
     
     @popular_male_names = PopularMaleName.all
     @popular_female_names = PopularFemaleName.all
+
+    @name_days_months = NameDay.months.keys
+    @name_day_today = NameDay.celebration_today
+    @name_day_tomorrow = NameDay.celebration_tomorrow
   end
 end
