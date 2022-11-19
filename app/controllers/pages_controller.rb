327b1e581587bab858439e6432462bf3fc94b3aa
phoenixixixix <barchukoleg12@gmail.com>
2022-11-19 16:50:23 +0200

    Display first two Selections on main page instead Popular male/female names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,7 @@ class PagesController < ApplicationController
     @alphabet = Name::UK_LETTERS_LIST
     @origin_countries = OriginCountry.select(:id, :uk_title_plural)
     
-    @popular_male_names = PopularMaleName.all
-    @popular_female_names = PopularFemaleName.all
+    @selection_first, @selection_last = Selection.take(2)
 
     @name_days_months = NameDay.months.keys
     @name_day_today = NameDay.celebration_today
