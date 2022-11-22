4869fbabd236829e25b44aa095a8dea55051a3c6
phoenixixixix <barchukoleg12@gmail.com>
2022-11-22 18:48:24 +0200

    Add edit, update actions for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %w(show edit update)
   before_action :get_all_tags, except: :show
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -22,10 +21,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :body, :tag_list)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def get_all_tags
