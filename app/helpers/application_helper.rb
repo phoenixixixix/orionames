a97f65aee573d17029b197557fe5226615e35d06
phoenixixixix <barchukoleg12@gmail.com>
2022-11-22 18:49:35 +0200

    Configure redcarpet's Markdown and display Post body using this helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def markdown(text)
+    options = [:hard_wrap, :autolink, :no_intra_emphasis, :underline, :highlight,
+               :no_images, :filter_html, :safe_links_only, :prettify, :no_styles]
+    Markdown.new(text, *options).to_html.html_safe
+  end
 end
