0ba7264d9d3793b892a05d41ab01f4963908daa6
phoenixixixix <barchukoleg12@gmail.com>
2022-12-04 14:41:57 +0200

    Display Selections:
    * Move selection to partial;
    * Apply styles;

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @alphabet = Name::UK_LETTERS_LIST
     @origin_countries = OriginCountry.select(:id, :uk_title_plural)
     
-    @selection_first, @selection_last = Selection.take(2)
+    @selections = Selection.take(2)
 
     @name_days_months = NameDay.months.keys
     @name_day_today = NameDay.celebration_today
