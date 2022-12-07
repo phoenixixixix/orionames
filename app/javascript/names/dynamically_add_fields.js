6a00dd08db093a2c51e6b3f57b50188345df99de
phoenixixixix <barchukoleg12@gmail.com>
2022-12-07 17:13:33 +0200

    Dynamically add fields using JS:
    * Add more fields for Femous People names attr on click to link;
    * Render page specific JS (PSJ) yielding head;

---
+++ app/javascript/names/dynamically_add_fields.js
@@ -0,0 +1,13 @@
+const add_famous_name = document.getElementById("add_famous_name")
+const famous_people_fields = document.getElementById("famous_people_fields")
+
+add_famous_name.onclick = () => {
+    let newField = document.createElement("input");
+    newField.setAttribute("class","form-control mb-1");
+    newField.setAttribute("multiple","multiple");
+    newField.setAttribute("type","text");
+    newField.setAttribute("name","name[famous_people_list_attributes][names][]");
+    newField.setAttribute("id","name_famous_people_list_attributes_names");
+
+    famous_people_fields.appendChild(newField)
+}
