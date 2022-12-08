b92792fa3f321f3c08341b9d9ecd467510e5b10d
phoenixixixix <barchukoleg12@gmail.com>
2022-12-08 13:52:01 +0200

    Improve Dynamically adding fields:
    * Add ability to edit and update Names;
    * Add missing attributes that should be permitted in names params to propperly update wiki and famous_people_list;
    * Refactor add famous_people_list form field to populate it acording to names array attr;
    * Update the newField variable build in dynamically_add_fields to accord Updated form field;

--- app/controllers/names_controller.rb
+++ app/controllers/names_controller.rb
@@ -1,4 +1,6 @@
 class NamesController < ApplicationController
+  before_action :set_name, only: %w(show edit update)
+
   def index
     @names = filtered_names(Name.all, filter_params)
     @names_hash = @names.group_by(&:capital_letter).sort_by { |letter, _| letter }.to_h
@@ -12,7 +14,6 @@ class NamesController < ApplicationController
   end
 
   def show
-    @name = Name.find(params[:id])
     @wiki = @name.wiki
   end
 
@@ -31,11 +32,28 @@ class NamesController < ApplicationController
     end
   end
 
+  def edit
+    @name.wiki
+    @name.famous_people_list ? @name.famous_people_list : @name.build_famous_people_list
+  end
+
+  def update
+    if @name.update(name_params)
+      redirect_to @name
+    else
+      render "edit"
+    end
+  end
+
   private
+
+  def set_name
+    @name = Name.find(params[:id])
+  end
 
   def name_params
     params.require(:name).permit(:title, :category, :origin_country_id, 
-      wiki_attributes: [:origin, :meaning], famous_people_list_attributes: [names: []])
+      wiki_attributes: [:origin, :meaning, :id], famous_people_list_attributes: [:id, names: []])
   end
 
   def filter_params
--- app/javascript/names/dynamically_add_fields.js
+++ app/javascript/names/dynamically_add_fields.js
@@ -3,11 +3,10 @@ const famous_people_fields = document.getElementById("famous_people_fields")
 
 add_famous_name.onclick = () => {
     let newField = document.createElement("input");
-    newField.setAttribute("class","form-control mb-1");
-    newField.setAttribute("multiple","multiple");
     newField.setAttribute("type","text");
+    newField.setAttribute("class","form-control mb-1");
     newField.setAttribute("name","name[famous_people_list_attributes][names][]");
-    newField.setAttribute("id","name_famous_people_list_attributes_names");
+    newField.setAttribute("id","name_famous_people_list_attributes_names_");
 
     famous_people_fields.appendChild(newField)
 }
