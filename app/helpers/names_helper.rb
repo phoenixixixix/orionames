0714b0dbb8e31f6cd5048fb59c80e930dfa1267a
phoenixixixix <barchukoleg12@gmail.com>
2022-12-11 20:22:30 +0200

    Move filters links from names#index view to helper

--- app/helpers/names_helper.rb
+++ app/helpers/names_helper.rb
@@ -1,4 +1,9 @@
 module NamesHelper
+  def link_to_with_filter_multiplier(param_key, param_value, **html_options)
+    # except :page parameter to avoid pages without names
+    link_to param_value, request.params.merge("#{param_key}" => param_value).except(:page), class: html_options[:class]
+  end
+  
   def build_heading_from_filters(filters)
     category, origin, letter = filters[:by_category], filters[:by_origin], filters[:by_letter]
 
