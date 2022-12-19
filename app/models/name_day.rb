106482d98d363dab73edf5d4d9c0a11661eafff2
phoenixixixix <barchukoleg12@gmail.com>
2022-12-19 22:19:14 +0200

    Add validations for NameDay:
    * Validate uniqueness of created Name Day by day, month;
    * Validate fullness of names_list;
    * Remove white spaces and empty strings from names_list;

--- app/models/name_day.rb
+++ app/models/name_day.rb
@@ -3,9 +3,12 @@ class NameDay < ApplicationRecord
                 august: 8, september: 9, october: 10, november: 11, december: 12 }
   enum celebration_status: { empty: 0, today: 1, tomorrow: 2 }
 
+  before_validation :remove_white_spaces_and_empty_strings
   before_save -> { names_list.uniq!}
 
   validates :day, :month, presence: true
+  validate :name_day_uniqueness, on: :create
+  validate :names_list_fullness
 
   scope :by_month, ->(month) { where(month: month) }
 
@@ -19,5 +22,19 @@ class NameDay < ApplicationRecord
 
   def human_day_month
     "#{day} #{month}"
+  end
+
+  private
+
+  def name_day_uniqueness
+    errors.add(:base, "Name Day already exists") if NameDay.find_by(day: day, month: month)
+  end
+
+  def names_list_fullness
+    errors.add(:names_list, "name list empty") if names_list.empty?
+  end
+
+  def remove_white_spaces_and_empty_strings
+    names_list.each { |name| name.strip! }.delete("")
   end
 end
