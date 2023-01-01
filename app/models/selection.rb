394acfdb4cf767d269960fd03179a36ce1cd07b4
phoenixixixix <barchukoleg12@gmail.com>
2023-01-01 14:56:03 +0200

    Test Selection model + Add new validation:
    * Validation and custom method tests;
    * Add validation to avoid invalid key format in names hash;

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -5,6 +5,7 @@ class Selection < ApplicationRecord
 
   validates :title, presence: true
   validate :names_exactly_10
+  validate :numeric_key_in_names
 
   scope :pinned, ->{ where(pinned: true) }
 
@@ -17,5 +18,15 @@ class Selection < ApplicationRecord
 
   def names_exactly_10
     errors.add(:names, "Names number should be exactly 10") if names.count != 10
+  end
+
+  def numeric_key_in_names
+    return unless names
+    names.each do |id, _|
+      if id.match?(/\D/)
+        errors.add(:names, "Hash keys should be numeric")
+        break
+      end
+    end
   end
 end
