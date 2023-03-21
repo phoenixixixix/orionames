de22085d86939dda3525250e9a66ae17dde1d307
phoenixixixix <barchukoleg12@gmail.com>
2023-03-21 21:29:54 +0200

    Breadcrambs for error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,18 @@
 class ErrorsController < ApplicationController
+  add_breadcrumb I18n.t("breadcrumbs.main_page"), :root_path
+
   def not_found
+    add_breadcrumb I18n.t("breadcrumbs.404_page")
     render status: 404
   end
 
   def unacceptable
+    add_breadcrumb I18n.t("breadcrumbs.422_page")
     render status: 422
   end
 
   def internal_server_error
+    add_breadcrumb I18n.t("breadcrumbs.500_page")
     render status: 500
   end
 end
