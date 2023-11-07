70da59ef68c54c82d1a67f782169a9d88e513846
phoenixixixix <barchukoleg12@gmail.com>
2023-11-07 17:57:05 +0200

    Specs for Name callbacks and order_by_capital_letter scope

--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -1,6 +1,56 @@
 require "rails_helper"
 
 RSpec.describe Name, type: :model do
+  describe "callbacks" do
+    let(:name) { create(:name, title: title) }
+    let(:title) { "Дефолт" }
+
+    describe "capitalize title" do
+      subject { name.title.first }
+
+      context "when title in lower case" do
+        let(:title) { "тест" }
+
+        it "expects title starts with capital" do
+          expect(subject).to eq("Т")
+        end
+      end
+    end
+
+    describe "#set_capital_letter" do
+      subject { name.capital_letter }
+
+      let(:title) { "Саша" }
+
+      it "sets capital letter field" do
+        expect(subject).to eq(title.first)
+      end
+    end
+  end
+
+  describe ".order_by_capital_letter" do
+    subject { described_class.order_by_capital_letter }
+
+    before { Name.delete_all }
+
+    it "sorts names by Ukrainian alphabet" do
+      greater_in_list = create(:name, title: "А")
+      lower_in_list = create(:name, title: "Ф")
+      middle_in_list = create(:name, title: "Б")
+
+      expect(subject).to eq([greater_in_list, middle_in_list, lower_in_list])
+    end
+
+    context "when persisted name with capital letter out of Ukrainian alphabet" do
+      it "should place it in the end of the list" do
+        create(:name, title: "А")
+        lower_in_list = create(:name, title: "$Nomatch")
+
+        expect(subject.last).to eq(lower_in_list)
+      end
+    end
+  end
+
   describe ".apply_filters" do
     subject { described_class.apply_filters(filter) }
 
